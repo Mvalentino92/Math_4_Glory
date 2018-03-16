d946b64e6e7b9d1199cffbf269e9092c63857021
Michael Valentino <michael.valentino92@gmail.com>
2018-03-16 17:31:59 -0400

    Add files via upload

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -1,4 +1,4 @@
-/* Lists the prime factors of a number in decreasing order */
+/* Lists the prime factors of a number in non decreasing order */
 
 import java.util.*; 
 import java.math.*; 
@@ -6,19 +6,40 @@ import java.io.*;
 
 public class PrimeFactors
 {
+	//Constant's I'll need.
+	public static final BigInteger ZERO = BigInteger.ZERO;
+	public static final BigInteger ONE = BigInteger.ONE;
+	public static final BigInteger TWO = new BigInteger("2");
+	public static final BigInteger THREE = new BigInteger("3");
+
+	//Double checks to make sure the factor is prime.
+	public static boolean isPrime(BigInteger number)
+	{
+		if(number.mod(TWO).equals(ZERO)) return number.divide(TWO).equals(ONE) ? true : false;
+		BigInteger divisor = THREE;
+		while(number.divide(divisor).compareTo(divisor) != -1)
+		{
+			if(number.mod(divisor).equals(ZERO)) return false;
+			divisor = divisor.add(TWO);
+		}
+		return true;
+	}
+			
+	//Prints the prime factors of a number.
 	public static void printFactors(BigInteger number)
 	{
-		if(number.equals(BigInteger.ONE)) 
+		if(number.equals(ONE)) 
 		{
 			System.out.println();
 			return;
 		}
 
-		for(BigInteger i = new BigInteger("2"); i.compareTo(number) != 1; i = i.add(BigInteger.ONE))
+		for(BigInteger i = TWO; i.compareTo(number) != 1; i = i.add(ONE))
 		{
-			if(number.mod(i).equals(BigInteger.ZERO))
+			if(number.mod(i).equals(ZERO))
 			{
-				System.out.print(i+" ");
+				if(isPrime(i)) System.out.print(i+" ");
+				else System.out.print("X ");
 				printFactors(number.divide(i));
 				return;
 			}
